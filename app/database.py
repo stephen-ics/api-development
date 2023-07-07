3610d8f12b0510218db1a034b9e1db454472f724
stephen-ics <stephenni1234@gmail.com>
2023-07-07 16:17:14 -0400

    Cleaning up code

--- app/database.py
+++ app/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import psycopg
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:password123@localhost/apis'
 
@@ -16,3 +17,12 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# Connecting db using raw sql instead of sqlalchemy
+# try:
+#    conn = psycopg.connect("dbname=apis user=postgres password=password123")
+#    cursor = conn.cursor()
+#    print('Connected to database!')
+# except Exception as error:
+#    print('Connecting to database failed')
+#    print('Error', error)
--- app/main.py
+++ app/main.py
@@ -1,25 +1,11 @@
-from fastapi import FastAPI, Response, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel, EmailStr
-from typing import Optional, List
-from random import randrange
-import psycopg
-from .database import engine, get_db
-from . import models, schemas, utils
-from sqlalchemy.orm import Session
+from fastapi import FastAPI
+from .database import engine
+from . import models
 from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(debug=True)
-
-try:
-    conn = psycopg.connect("dbname=apis user=postgres password=password123")
-    cursor = conn.cursor()
-    print('Connected to database!')
-except Exception as error:
-    print('Connecting to database failed')
-    print('Error', error)
 
 app.include_router(post.router)
 app.include_router(user.router)
