80e35818a3d1ac434a0316523ded896bb4da0284
stephen-ics <stephenni1234@gmail.com>
2023-07-10 23:28:39 -0400

    parametrize + fixtures

--- app/calculations.py
+++ app/calculations.py
@@ -1,2 +1,12 @@
 def add(num1: int, num2: int):
     return num1 + num2
+
+class BankAccount():
+    def __init__(self, starting_balance=0):
+        self.balance = starting_balance
+    def deposite(self, amount):
+        self.balance += amount
+    def withdraw(self, amount):
+        self.balance -= amount
+    def collect_interest(self):
+        self.balance *= 1.1
