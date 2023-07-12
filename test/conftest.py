c87444b7027d825b2429d253df30da568bb764e5
stephen-ics <stephenni1234@gmail.com>
2023-07-12 16:22:58 -0400

    all user create/login tests + conftest.py

--- test/conftest.py
+++ test/conftest.py
@@ -8,13 +8,14 @@ from app.database import get_db
 from app.database import Base
 import pytest
 
+# Initialize + Drop DB
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}_test'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-@pytest.fixture
+@pytest.fixture()
 def session():
     Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
@@ -25,7 +26,8 @@ def session():
     finally:
         db.close()
 
-@pytest.fixture
+# Creating user used for login validation
+@pytest.fixture()
 def client(session):
     def override_get_db():
         try:
@@ -34,3 +36,18 @@ def client(session):
             session.close()
     app.dependency_overrides[get_db] = override_get_db
     yield TestClient(app)
+
+@pytest.fixture()
+def test_user(client):
+    user_data = {
+        'email': 'hellooo@gmail.com',  
+        'password': 'password123'}
+    
+    res = client.post('/users/', json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+
+    return new_user
+
+    
--- test/test_users.py
+++ test/test_users.py
@@ -1,5 +1,8 @@
 from app import schemas
-from .database import client, session
+from fastapi import status
+from jose import jwt
+import pytest
+from app.config import settings
 
 def test_root(client):
     res = client.get('/') 
@@ -12,3 +15,36 @@ def test_create_user(client):
     new_user = schemas.UserResponse(**res.json())
     assert new_user.email == 'hellos1234@gmail.com'
     assert res.status_code == 201
+
+def test_login_user(test_user, client):
+    res = client.post(
+        "/login", json={'email': test_user['email'], 'password': test_user['password']}
+    )
+
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms=[settings.algorithm])
+    id = payload.get('user_id')
+
+    assert id == test_user['id']
+    assert login_res.token_type == 'bearer'
+    assert res.status_code == 200
+
+@pytest.mark.parametrize('email, password, status_code', [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('hellooo@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('hellooo@gmail.com', None, 422)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+
+    res = client.post(
+        '/login', json={'email': email, 'password': password}
+    )
+
+    assert res.status_code == status_code
+
+
+
+                                                                                                                                   
+                                                                   
