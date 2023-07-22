8303fd6435456dd23617d75863d2ba98785741ba
stephen-ics <stephenni1234@gmail.com>
2023-07-21 23:01:22 -0400

    added get main posts vs get all posts

--- app/routers/post.py
+++ app/routers/post.py
@@ -21,7 +21,23 @@ def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.
     # posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all() --> The unjoined version
 
     posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
-        models.Vote, models.Vote.post_id == models.Post.id, isouter = True).group_by(models.Post.id).filter(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter = True).group_by(models.Post.id).filter(models.Post.parent_id == None).filter(
+        models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    
+    return posts
+
+@router.get('/main', response_model=List[schemas.PostResponse])
+def get_all_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), limit: int = 10, skip: int = 0, search: Optional[str] = ""):
+    # cursor.execute("""
+    #    SELECT *
+    #    FROM posts
+    # """)
+    # posts = cursor.fetchall()
+
+    # posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all() --> The unjoined version
+
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter = True).group_by(models.Post.id).filter(models.Post.parent_id == None).filter(
         models.Post.title.contains(search)).limit(limit).offset(skip).all()
     
     return posts
