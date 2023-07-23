bd05371c1d9cf4762de01e5cd76359dfa86f98e0
stephen-ics <stephenni1234@gmail.com>
2023-07-22 23:35:59 -0400

    added bio and pfp to users table

--- app/models.py
+++ app/models.py
@@ -24,6 +24,8 @@ class User(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
+    biography = Column(String, nullable=True)
+    profile_photo = Column(String, nullable=True)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()')) 
